7b89bc1b5584914cb4a83e0c81d76f2306ee206c
rohanprakashpawar2005 <rohanprakashpawar2005@gmail.com>
2024-10-02 08:39:18 +0000

    First commit

---
+++ .idx/dev.nix
@@ -0,0 +1,29 @@
+{pkgs}: {
+  channel = "stable-23.11";
+  packages = [
+    pkgs.nodejs_20
+    pkgs.libressl_3_6.bin
+    pkgs.apt
+  ];
+  idx.extensions = [
+    
+  ];
+  idx.previews = {
+    previews = {
+      web = {
+        command = [
+          "npm"
+          "run"
+          "dev"
+          "--"
+          "--port"
+          "$PORT"
+          "--hostname"
+          "0.0.0.0"
+        ];
+        manager = "web";
+      };
+    };
+  };
+}
+
